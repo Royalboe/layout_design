b1f205daf20a95b4a74448e15261927370ae9a52
Royalboe <auomidejohnson@gmail.com>
2022-02-09 22:44:21 +0100

    Added a new activity and layout design

--- app/src/main/java/com/royalboe/layoutdesigns/MainActivity.kt
+++ app/src/main/java/com/royalboe/layoutdesigns/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.royalboe.layoutdesigns
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.login_button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, WeatherActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
